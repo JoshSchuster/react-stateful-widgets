db6cac58fbf33df34ae5bcd740232f239ca145d0
JS MacBook Pro <jsmacbookpro@JSs-MacBook-Pro.local>
2020-11-17 23:50:42 -0600

    competes the rest of steps in Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -55,12 +55,15 @@ console.log('count', count)
 
   const increment = () => {
     /* STEP 4 */
+    setCount(count +1)
   };
   const decrement = () => {
     /* STEP 5 */
+    setCount(count - 1)
   };
   const reset = () => {
     /* STEP 6 */
+    setCount(0)
   };
 
   const style = {
