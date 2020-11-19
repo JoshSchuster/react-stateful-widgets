afaf69fe51446c183be4a5620143c980116c2bec
JS MacBook Pro <jsmacbookpro@JSs-MacBook-Pro.local>
2020-11-18 21:10:04 -0600

    completes steps in Moods.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -56,13 +56,12 @@ export default function Input() {
     fontSize: '1.5em',
     marginBottom: '0.3em',
     color: (inputValue.length <= 10) ? 'royalblue' : 'crimson', /* STEP 2 */
-    textTransform: 'uppercase'
   };
 
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-      <div id='output' style={style}>{inputValue}</div> {/* STEP 3 */}
+      <div id='output' style={style}>{inputValue.toUpperCase()}</div> {/* STEP 3 */}
       <div>
         <input id='input' type='text' onChange={changeInput} value={inputValue}/> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
